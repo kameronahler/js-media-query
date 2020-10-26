6d0797a39bbbfeeb34cd5036e76fe98bcb511e04
Kameron <kamahler@gmail.com>
2020-10-25 20:48:35 -0600

    add basic listeners without logic

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,7 +2,22 @@ import React, { useState } from 'react'
 import AppHeader from '../AppHeader/AppHeader'
 
 const App = () => {
-  const [text, setText] = useState('Parcel + React is pretty spiffy')
+  const [text, setText] = useState(() => updateText)
+
+  const sm = window.matchMedia('(min-width: 600px)')
+  const md = window.matchMedia('(min-width: 1024px)')
+  const lg = window.matchMedia('(min-width: 1440px)')
+  const xl = window.matchMedia('(min-width: 1920px)')
+
+  const updateText = (event) => {
+    console.log(event.media)
+    setText(event.media)
+  }
+
+  sm.addListener(updateText)
+  md.addListener(updateText)
+  lg.addListener(updateText)
+  xl.addListener(updateText)
 
   return (
     <>
