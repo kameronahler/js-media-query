73fcd42e2064de8ec78f4fc6f5848fb746e24915
Kameron <kamahler@gmail.com>
2020-10-25 19:08:26 -0600

    parcel and react

---
+++ src/components/app/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <header>
+        <h1>Parcel + React is pretty spiffy</h1>
+      </header>
+    </main>
+  )
+}
+
+export { App as default }
