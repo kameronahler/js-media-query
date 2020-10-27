4f3fd467b944e02892c2776af1f526a1d74bfcda
Kameron <kamahler@gmail.com>
2020-10-27 08:58:41 -0600

    the state updates when expected now

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,16 +2,24 @@ import React, { useState } from 'react'
 import AppHeader from '../AppHeader/AppHeader'
 
 const App = () => {
-  const [text, setText] = useState(() => updateText)
-
+  const [text, setText] = useState(() => 'load')
   const sm = window.matchMedia('(min-width: 600px)')
   const md = window.matchMedia('(min-width: 1024px)')
   const lg = window.matchMedia('(min-width: 1440px)')
   const xl = window.matchMedia('(min-width: 1920px)')
 
-  const updateText = (event) => {
-    console.log(event.media)
-    setText(event.media)
+  const updateText = () => {
+    if (xl.matches) {
+      setText('xl')
+    } else if (lg.matches) {
+      setText('lg')
+    } else if (md.matches) {
+      setText('md')
+    } else if (sm.matches) {
+      setText('sm')
+    } else {
+      setText('mobile')
+    }
   }
 
   sm.addListener(updateText)
