f5d25215bace73b0bfb7cc7a288247b76d338f27
Kameron <kamahler@gmail.com>
2020-10-30 08:50:28 -0600

    start a basic animation

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,38 +1,10 @@
-import React, { useState } from 'react'
+import React from 'react'
 import AppHeader from '../AppHeader/AppHeader'
 
 const App = () => {
-  const breakpointSM = window.matchMedia('(min-width: 600px)')
-  const breakpointMD = window.matchMedia('(min-width: 1024px)')
-  const breakpointLG = window.matchMedia('(min-width: 1440px)')
-  const breakpointXL = window.matchMedia('(min-width: 1920px)')
-  const breakpoints = [breakpointSM, breakpointMD, breakpointLG, breakpointXL]
-
-  const updateTextState = () => {
-    if (breakpointXL.matches) {
-      return 'xl'
-    } else if (breakpointLG.matches) {
-      return 'lg'
-    } else if (breakpointMD.matches) {
-      return 'md'
-    } else if (breakpointSM.matches) {
-      return 'sm'
-    } else {
-      return 'mobile'
-    }
-  }
-
-  breakpoints.forEach((bp) => {
-    bp.addListener(() => setText(updateTextState))
-  })
-
-  const [text, setText] = useState(() => {
-    return updateTextState()
-  })
-
   return (
     <>
-      <AppHeader text={text} />
+      <AppHeader />
       <main></main>
     </>
   )
